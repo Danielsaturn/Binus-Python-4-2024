70a6e9c60dd89984970c4ac1f997cab7a3345a96
Danielsaturn <danielbussiness2000@gmail.com>
2024-12-07 15:38:08 +0700

    Create Tugas while 3.py

---
+++ Tugas while 3.py
@@ -0,0 +1,35 @@
+tampungnilai = 0
+tampunginput = 0
+
+while (True):
+    inputan = input("Masukan Nilai: ")
+    if (inputan == ""):
+        
+        hasil = tampungnilai / tampunginput
+        break
+    
+    else;
+        if (inputan == "A"):
+            
+            tampungnilai = tampungnilai + 4.00
+            tampunginput =
+            
+        elif inputan == "A-"):
+            
+            tampungnilai = tampungnilai + 3.75
+            tampunginput =
+            
+        elif inputan == "B+"):
+            
+            tampungnilai = tampungnilai + 3.50
+            tampunginput =
+            
+        elif inputan == "B"):
+            
+            tampungnilai = tampungnilai + 3.00
+            tampunginput =
+            
+        elif inputan == "B-"):
+            
+            tampungnilai = tampungnilai + 2.75
+            tampunginput =
