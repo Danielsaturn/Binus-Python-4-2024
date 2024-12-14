b90b257545fb82607a702ccaaedf2043587543e9
Danielsaturn <danielbussiness2000@gmail.com>
2024-12-14 13:11:36 +0700

    Update Tugas for 2.py

--- Tugas for 2.py
+++ Tugas for 2.py
@@ -6,7 +6,7 @@ for row in range(usernum, 0, -1):
     for j in range(1, row + 1):
         print(row, end="")
     print()
-for i in range(1, usernum + 1):
+for i in range(2, usernum + 1):
     
     for j in range(1, i + 1):
         print(i, end="")
